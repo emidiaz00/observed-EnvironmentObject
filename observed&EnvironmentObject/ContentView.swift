bface3bdb30f940f9c1e8d3bdc566fc3182978f1
emildsn <emildsn00@gmail.com>
2023-05-30 17:34:53 -0300

    add observable

--- observed&EnvironmentObject/ContentView.swift
+++ observed&EnvironmentObject/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var numberRamdom = ramdomNumber()
+    var valor = 3
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("\(self.numberRamdom.number)")
+                .font(.title)
+                .bold()
+            if self.numberRamdom.number == self.valor {
+                Text("Es igual")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(.indigo)
+            } else {
+                Text("Es diferente")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(.red)
+            }
+            HStack {
+                Button(action: {
+                    self.numberRamdom.start()
+                }) {
+                    HStack {
+                        Image(systemName: "play.fill")
+                            .foregroundColor(.white)
+                        Text("Start")
+                            .foregroundColor(.white)
+                            .bold()
+                            .font(.headline)
+                    }.padding(.all)
+                }.background(Color.indigo)
+                    .cornerRadius(18)
+                Button(action: {
+                    self.numberRamdom.stop()
+                }) {
+                    HStack {
+                        Image(systemName: "stop.fill")
+                            .foregroundColor(.white)
+                        Text("Stop")
+                            .foregroundColor(.white)
+                            .bold()
+                            .font(.headline)
+                    }.padding(.all)
+                }.background(Color.red)
+                    .cornerRadius(18)
+            }
         }
-        .padding()
+        
     }
 }
 
---
+++ observed&EnvironmentObject/randomNumber.swift
@@ -0,0 +1,29 @@
+//
+//  randomNumber.swift
+//  observed&EnvironmentObject
+//
+//  Created by Emiliano Diaz on 30/05/2023.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class ramdomNumber: ObservableObject {
+    @Published var number : Int = 0
+    var timer = Timer()
+    
+    init() {
+        start()
+    }
+    
+    func start() {
+        self.timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) {
+            (timer) in self.number = Int.random(in: 0 ..< 5)
+        }
+    }
+    
+    func stop() {
+        timer.invalidate()
+    }
+}
