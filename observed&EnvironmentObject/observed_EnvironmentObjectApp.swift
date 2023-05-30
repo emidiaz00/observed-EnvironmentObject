f02163fe0eef350eee0e19bcadda599ebb6aa410
emildsn <emildsn00@gmail.com>
2023-05-30 16:56:43 -0300

    Initial Commit

---
+++ observed&EnvironmentObject/observed_EnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  observed_EnvironmentObjectApp.swift
+//  observed&EnvironmentObject
+//
+//  Created by Emiliano Diaz on 30/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct observed_EnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
